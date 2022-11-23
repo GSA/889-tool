5125648c61635df22ed4c1b86e0f5dd4dcec91c7
Mark Meyer <mark@photo-mark.com>
2022-11-23 12:46:46 -0900

    change build path

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  build: {
+    outDir: '../_site'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
