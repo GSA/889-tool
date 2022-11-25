c50e7d2e0910c94d94172eae27d5a07ca907772d
Mark Meyer <mark@photo-mark.com>
2022-11-25 13:47:27 -0900

    Add NotFound component and catch-all route for unknown routes

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ResultsView from "../views/ResultsView.vue";
+import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/search/:term/:page(\\d+)?",
       name: "search",
       component: ResultsView,
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        name: 'NotFound', 
+        component: NotFound 
     },
   ],
 });
