5e76d8f9fb7d084409667171ba3d4e86cfe99940
Mark Meyer <mark@photo-mark.com>
2022-12-05 14:47:29 -0900

    link header to root file

--- front-end/src/components/GSAHeader.vue
+++ front-end/src/components/GSAHeader.vue
@@ -2,8 +2,12 @@
     <header class="usa-header usa-header--extended " role="banner">
         <div class="usa-navbar border-bottom-1px">
             <div class="usa-logo" id="header-logo">
-                <img class="usa-logo-img" src="@/assets/images/gsa-logo-reverse.svg" alt="GSA Logo" title="U.S. General Services Administration">
-                <em class="usa-logo__text">889 Compliance Search</em>
+                <router-link to="/">
+                    <img class="usa-logo-img" src="@/assets/images/gsa-logo-reverse.svg" alt="GSA Logo" title="U.S. General Services Administration">
+                </router-link>
+                <router-link to="/">
+                    <em class="usa-logo__text">889 Compliance Search</em>
+                </router-link>
             </div>
         </div>
     </header>
