3d9250d4fe1d97b060b1ddc886433dfe67bf21f0
Mark Meyer <mark@photo-mark.com>
2023-06-29 08:36:52 -0800

    add port to run command and fix secrets name

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-gunicorn samtools.wsgi:app --workers 2 --worker-class uvicorn.workers.UvicornWorker
+gunicorn -b :$PORT samtools.wsgi:app --workers 2 --worker-class uvicorn.workers.UvicornWorker
