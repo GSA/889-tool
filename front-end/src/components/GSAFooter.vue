bd2661e4d64d386820699f002b3cafffc3c0f22e
Mark Meyer <mark@photo-mark.com>
2023-07-18 15:33:07 -0800

    update d&b langauge

--- front-end/src/components/GSAFooter.vue
+++ front-end/src/components/GSAFooter.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="usa-footer usa-footer--slim">
-    <div class="usa-footer__primary-section">
-      <div class="usa-footer__primary-container grid-row">
-        <div class="mobile-lg:grid-col-8">
-            <p class="font-sans-3xs">
-                The 889 Representations SAM Tool web application was originally developed by NASA.<br />
-                We encourage input! Please provide feedback and suggestions to <a class="text-primary-dark" href="mailto:gsa_smartpay@gsa.gov">gsa_smartpay@gsa.gov</a>
-            </p>
-
-        </div>
-       
+  <div class="usa-footer usa-footer--slim">
+  <div class="usa-footer__primary-section">
+    <div class="usa-footer__primary-container grid-row">
+      <div class="mobile-lg:grid-col-8">
+          <p class="font-sans-3xs">
+              The 889 Representations SAM Tool web application was originally developed by NASA.<br />
+              We encourage input! Please provide feedback and suggestions to <a class="text-primary-dark" href="mailto:gsa_smartpay@gsa.gov">gsa_smartpay@gsa.gov</a> <br />
+          </p>
+          <p class="font-sans-3xs">
+            <a class="text-primary-dark" href="https://sam.gov/content/about/disclaimers">Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B) Data</a>
+          </p>
       </div>
     </div>
+  </div>
 </div>
 </template>
