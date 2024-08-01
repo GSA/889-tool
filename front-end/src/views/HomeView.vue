65c05703a24a0c6dc046983404d2a11a4420813b
John Labbate <90406009+john-labbate@users.noreply.github.com>
2024-08-01 11:26:30 -0400

    Spell out FOUO (#284)
    * Spell out FOUO
    
    * Update strings.js
    
    * Update HomeView.vue

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -39,9 +39,7 @@ onMounted(() => {
               This tool only checks for Section 889 representations in an
               entity’s SAM.gov record. Only entities doing business above the
               Micro-Purchase Threshold (MPT) are required to register in
-              SAM.gov. Contracts that are classified/FOUO or contractors that do
-              not wish for their information to be publicly available will not
-              show up in search results.
+              SAM.gov. Contracts classified as For Official Use Only (FOUO) or contractors who do not wish for their information to be publicly available will not appear in search results.
             </p>
 
             <p>
