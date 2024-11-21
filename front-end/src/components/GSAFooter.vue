718e470ab7737e94a69d2a4fc9696f6bfe58288a
Dan Felder <d.felder@kunzleigh.com>
2024-11-21 10:48:50 -0500

    Updating touchpoint configuration

--- front-end/src/components/GSAFooter.vue
+++ front-end/src/components/GSAFooter.vue
@@ -1,16 +1,3 @@
-<script setup>
-
-import {onMounted} from "vue";
-
-onMounted (() => {
-  let recaptchaScript = document.createElement('script')
-  recaptchaScript.async = true;
-  recaptchaScript.setAttribute('src', 'https://touchpoints.app.cloud.gov/touchpoints/55c3c7c9.js')
-  document.head.appendChild(recaptchaScript)
-})
-
-</script>
-
 <template>
   <div class="usa-footer usa-footer--slim">
     <div class="usa-footer__primary-section">
@@ -36,12 +23,7 @@ onMounted (() => {
               Data</a>
           </p>
         </div>
-        <div class="mobile-lg:grid-col-4">
-          <button id="touchpoint-survey-889" class="usa-button margin-top-2 margin-bottom-2" type="button">Leave us your feedback</button>
-        </div>
       </div>
     </div>
   </div>
 </template>
-<script setup>
-</script>
