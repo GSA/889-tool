07107d19c01bf812edbc00680614d8005e14a232
Dan Felder <30899941+felder101@users.noreply.github.com>
2025-05-08 15:19:44 -0400

    Removed D&B data usage link (#514)

--- front-end/src/components/GSAFooter.vue
+++ front-end/src/components/GSAFooter.vue
@@ -15,13 +15,6 @@
             >gsa_smartpay@gsa.gov</a>.
             <br>
           </p>
-          <p class="font-sans-3xs">
-            <a
-              class="text-primary-dark"
-              href="https://sam.gov/content/about/disclaimers"
-            >Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B)
-              Data</a>
-          </p>
         </div>
       </div>
     </div>
