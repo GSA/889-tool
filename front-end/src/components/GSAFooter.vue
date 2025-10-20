e1eac86e6bd03b435aafd3145043685933b68ca2
Hieu P. So <hieupso@U-3QZER1TF9HFVD.gtd.gsa.gov>
2025-10-20 17:36:53 -0400

    resolve conflict v3

--- front-end/src/components/GSAFooter.vue
+++ front-end/src/components/GSAFooter.vue
@@ -4,7 +4,7 @@
       <div class="usa-footer__primary-container grid-row">
         <div class="mobile-lg:grid-col-8">
           <p class="font-sans-3xs">
-            This tool was built by NASA using the <a
+            This search was built by NASA using the <a
               class="text-primary-dark"
               href="https://sam.gov/content/home"
           >SAM.gov</a> Entity Management API.<br>
@@ -14,13 +14,6 @@
               href="mailto:gsa_smartpay@gsa.gov"
             >gsa_smartpay@gsa.gov</a>.
             <br>
-          </p>
-          <p class="font-sans-3xs">
-            <a
-              class="text-primary-dark"
-              href="https://sam.gov/content/about/disclaimers"
-            >Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B)
-              Data</a>
           </p>
         </div>
       </div>
