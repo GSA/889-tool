67d311067d38295506ab14e09e4f49f2fb7dc429
hieu so <hieu.so@gsa.gov>
2025-12-24 12:52:33 -0500

    prod releae

--- front-end/src/strings.js
+++ front-end/src/strings.js
@@ -6,6 +6,4 @@ const NO_RESULT_MESSAGE = `
     <br/><br /><b>Please Note:</b> If an entity representation is not available, cardholders may still be permitted to purchase from a listed entity. However, the cardholder would be responsible for documenting compliance with Section 889 in accordance with any applicable agency requirements.`;
 const API_ERROR_MESSAGE =
   "Sorry, we weren't able to connect to SAM.gov. Please try again later.";
-const SEARCH_ISSUE_MESSAGE ="We are aware that some users are experiencing issues with search functionality on the 889 tool. The GSA technical team is working to resolve this and ensure accurate search results. We appreciate your patience.";
-
-export { OK_TEXT, WARN_TEXT, NO_RESULT_MESSAGE, API_ERROR_MESSAGE, SEARCH_ISSUE_MESSAGE };
+export { OK_TEXT, WARN_TEXT, NO_RESULT_MESSAGE, API_ERROR_MESSAGE };
